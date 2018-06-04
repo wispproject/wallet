b733d520499b09b9827afcb9cca9b80325443202
wisp3r <wisp3r@protonmail.com>
2018-06-04 12:59:38 +0800

    Revert "initial implementation of BUIP-HF Digest for replay protection across forks"
    This reverts commit b0cf334cdaebfe22635d87ca79ddb40733d5f9ff.

--- src/test/script_tests.cpp
+++ src/test/script_tests.cpp
@@ -406,7 +406,7 @@ BOOST_AUTO_TEST_CASE(script_combineSigs)
 
     // A couple of partially-signed versions:
     vector<unsigned char> sig1;
-    uint256 hash1 = SignatureHash(scriptPubKey, txTo, 0, SIGHASH_ALL | SIGHASH_FORKID);
+    uint256 hash1 = SignatureHash(scriptPubKey, txTo, 0, SIGHASH_ALL);
     BOOST_CHECK(keys[0].Sign(hash1, sig1));
     sig1.push_back(SIGHASH_ALL);
     vector<unsigned char> sig2;
