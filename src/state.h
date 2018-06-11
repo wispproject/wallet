3543806ff529ac05c0ba43a7c9707368b5c9058d
Gandalf86 <bitcoinx2@users.noreply.github.com>
2018-06-11 19:02:01 +0200

    Adjust copyright statements

--- src/state.h
+++ src/state.h
@@ -1,5 +1,6 @@
 // Copyright (c) 2014 The ShadowCoin developers
-// Copyright (c) 2016 The SpectreCoin developers
+// Copyright (c) 2016-2018 The SpectreCoin developers
+// Copyright (c) 2018 The Wisp developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file license.txt or http://www.opensource.org/licenses/mit-license.php.
 
